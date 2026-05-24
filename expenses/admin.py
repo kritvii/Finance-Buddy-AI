b8549cd6391341eca0fc192eaa2be30e1348f996
Kritvi Rawat <kritvirawat@gmail.com>
2026-05-25 02:19:29 +0530

    Add customizable income and savings goal

--- expenses/admin.py
+++ expenses/admin.py
@@ -1,41 +1,14 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import User, ExpenseEntry
 
-@admin.register(User)  #Show this model inside Django admin panel
+@admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    """Admin interface for Users."""
-    list_display = ('username', 'email', 'monthly_income', 'created_at')
-    list_filter = ('created_at',)
-    search_fields = ('username', 'email') #searchbar functionality 
-    fieldsets = (
-        ('Personal Info', {
-            'fields': ('username', 'email', 'first_name', 'last_name')
-        }),
-        ('Financial Info', {
-            'fields': ('monthly_income',)
-        }),
-        ('Permissions', {
-            'fields': ('is_active', 'is_staff', 'is_superuser')
-        }),
-    )
-
+    list_display = ['username', 'email', 'monthly_income', 'savings_goal']
+    list_filter = ['date_joined']
+    search_fields = ['username', 'email']
 
 @admin.register(ExpenseEntry)
 class ExpenseEntryAdmin(admin.ModelAdmin):
-    """Admin interface for Expenses."""
-    list_display = ('user', 'amount', 'category', 'date', 'description')
-    list_filter = ('category', 'date', 'user')
-    search_fields = ('user__username', 'description') #user__username-->Go through ForeignKey relationship and search username field
-    date_hierarchy = 'date'
-    fieldsets = (
-        ('Expense Info', {
-            'fields': ('user', 'amount', 'category', 'description')
-        }),
-        ('Date Info', {
-            'fields': ('date',),
-        }),
-    )
-
-    
+    list_display = ['user', 'amount', 'category', 'date']
+    list_filter = ['category', 'date']
+    search_fields = ['description', 'user__username']
--- expenses/serializers.py
+++ expenses/serializers.py
@@ -1,44 +1,17 @@
 from rest_framework import serializers
-from django.contrib.auth import get_user_model
-from .models import ExpenseEntry
-
-User = get_user_model()
-
+from .models import User, ExpenseEntry
 
 class UserSerializer(serializers.ModelSerializer):
-    """Serializer for User model."""
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'monthly_income', 'created_at']
-        read_only_fields = ['id', 'created_at']
-
+        fields = ['id', 'username', 'email']
 
 class UserDetailSerializer(serializers.ModelSerializer):
-    """Detailed user info for signup and updates."""
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'monthly_income', 'password']
-        read_only_fields = ['id']
-        extra_kwargs = {
-            'password': {'write_only': True}
-        }
-    
-    def create(self, validated_data):
-        """Create new user with hashed password."""
-        user = User.objects.create_user(**validated_data)
-        return user
-
+        fields = ['id', 'username', 'email', 'monthly_income', 'savings_goal']
 
 class ExpenseEntrySerializer(serializers.ModelSerializer):
-    """Serializer for ExpenseEntry model."""
-    user = UserSerializer(read_only=True)
-    user_id = serializers.IntegerField(write_only=True, required=False)
-    
     class Meta:
         model = ExpenseEntry
-        fields = ['id', 'user', 'user_id', 'amount', 'category', 'description', 'date', 'created_at']
-        read_only_fields = ['id', 'created_at']
-    
-    def create(self, validated_data):
-        """Create expense entry."""
-        return ExpenseEntry.objects.create(**validated_data)
+        fields = ['id', 'user', 'amount', 'category', 'description', 'date']
